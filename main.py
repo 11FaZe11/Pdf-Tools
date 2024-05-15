976007f77fc0c26d21c4dd99a37f766b5407cdf2
Abdo Zakaria <148268203+11FaZe11@users.noreply.github.com>
2024-05-15 04:08:37 +0300

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from reportlab.pdfgen import canvas
 from docx2pdf import convert
 from pdf2docx import Converter
 import time
-## Run in your terminal >>>> pip install PyPDF2 docx2pdf pdf2docx 
+from tkinter import *
+from tkinter import filedialog, messagebox
+from PyPDF2 import PdfFileReader, PdfReader, PdfWriter
+
 
 
 while True:
@@ -16,13 +19,14 @@ while True:
 1- Convert PDF to WORD file.
 2- Convert WORD to PDF file.
 3- Add numbers on PDF file.
+4- PDF locker tool.
 0- End the program.
 """)
 
     first_choice = int(input("\nEnter the operation you wish..... \n"))
 
 
-    if first_choice in range(0,4):
+    if first_choice in range(0,10):
 
         if first_choice == 1:
             print("""Welcome to the PDF to WORD converter""")
@@ -63,7 +67,6 @@ while True:
 
 
             convert_word_to_pdf()
-
 
 
 
@@ -114,6 +117,73 @@ while True:
             add_page_numgers()
 
 
+
+        elif first_choice == 4:
+            print("""Welcome to the PDF locker tool""")
+            def select_source_file():
+                file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+                if file_path:
+                    source.set(file_path)
+                    target.set(file_path[:-4] + "_locked.pdf")
+
+
+            def protect_pdf():
+                input_pdf_path = source.get()
+                output_pdf_path = target.get()
+
+                if not input_pdf_path or not output_pdf_path:
+                    messagebox.showerror("Error", "Please select source and target PDF files.")
+                    return
+
+                password = password_entry.get()
+                if not password:
+                    messagebox.showerror("Error", "Please enter a password.")
+                    return
+
+                pdf_writer = PdfWriter()
+                pdf_reader = PdfReader(input_pdf_path)
+
+                for page_num in range(len(pdf_reader.pages)):
+                    pdf_writer.add_page(pdf_reader.pages[page_num])
+
+                pdf_writer.encrypt(user_pwd=password, use_128bit=True)
+
+                with open(output_pdf_path, 'wb') as encrypted_pdf_file:
+                    pdf_writer.write(encrypted_pdf_file)
+
+                messagebox.showinfo("Success", "PDF encrypted and saved successfully.")
+
+
+            root = Tk()
+            root.title("PDF Locker App")
+            root.geometry("700x400")
+            root.resizable(True, True)
+
+            frame = Frame(root)
+            frame.pack(padx=20, pady=20)
+
+            source = StringVar()
+            target = StringVar()
+
+            Label(frame, text="Source File PDF:", font="arial 10 bold").grid(row=0, column=0, sticky=W)
+            entry1 = Entry(frame, width=30, textvariable=source, font='arial 15', bd=1)
+            entry1.grid(row=0, column=1)
+            Button(frame, text="Select", command=select_source_file).grid(row=0, column=2)
+
+            Label(frame, text="Target PDF file:", font="arial 10 bold").grid(row=1, column=0, sticky=W)
+            entry2 = Entry(frame, width=30, textvariable=target, font='arial 15', bd=1)
+            entry2.grid(row=1, column=1)
+
+            Label(frame, text="Enter Password:", font="arial 10 bold").grid(row=2, column=0, sticky=W)
+            password_entry = Entry(frame, width=30, font='arial 15', bd=1)
+            password_entry.grid(row=2, column=1)
+
+            Button(frame, text="Lock PDF", command=protect_pdf).grid(row=3, column=1, pady=20)
+
+            root.mainloop()
+
+
+
         elif first_choice == 0:
             break
 
@@ -122,7 +192,7 @@ while True:
     print("""
         the program will start in 3 seconds
         """)
-    for abc in range(1,2):
+    for abc in range(1,3):
         print(abc)
         time.sleep(1)
 
